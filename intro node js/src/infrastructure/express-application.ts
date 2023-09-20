c121601e977f12b6e609cc3a135eb64ef6e1a496
Ryskell <samhauho2001@gmail.com>
2023-09-20 16:02:48 +0200

    a
    a

---
+++ intro node js/src/infrastructure/express-application.ts
@@ -0,0 +1,10 @@
+import { ExpressServer } from "./express-server";
+
+export class ExpressApplication {
+    private server = new ExpressServer('3000');
+
+    bootstrap(): void {
+        this.server.bootstrap();
+    
+    }
+}
